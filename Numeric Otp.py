ea9b0e00026d413dbfe006e1777e5c61af4f330e
MD Saifuzzaman Sohan <107489012+MSSohan@users.noreply.github.com>
2022-06-17 00:12:38 +0600

    Code

---
+++ Numeric Otp.py
@@ -0,0 +1,10 @@
+import math, random
+def generateOTP():
+  string = '0123456789'
+  OTP = ""
+  length = len(string)
+  for i in range(6):
+        OTP += string[math.floor(random.random() * length)]
+  return OTP
+if __name__ == "__main__":
+  print("OTP of length 6:", generateOTP())
